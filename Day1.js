29faa65c0c08e39f057bd49198b0f1a6362ebd05
Abhishek <abhishekfbj@gmail.com>
2022-12-01 23:10:59 +0530

    Copied from folder

---
+++ Day1.js
@@ -0,0 +1,20 @@
+colors = [
+    {id: 1001, name: "Black"},
+    {id: 1002, name: "Blue"},
+    {id: 1003, name: "White"},
+    {id: 1004, name: "Green"},
+]
+SubColor = [
+    {id: 2001, name: "Dim Gray", colorId:1001},
+    {id: 2002, name: "Cool Black", colorId:1001},
+    {id: 2003, name: "Cream", colorId:1003},
+    {id: 2004, name: "EggShell", colorId:1003},
+    {id: 2005, name: "Neon Blue", colorId:1002}
+    {id: 2006, name: "Dark Blue", colorId:1002},
+    {id: 2007, name: "Light Green", colorId:1004},
+    {id: 2008, name: "Dark Green", colorId:1004},
+]
+
+
+
+// https://stackblitz.com/edit/angular-ivy-h9tvdp?file=src%2Fapp%2Fapp.component.html%3AL17,src%2Fapp%2Fapp.component.ts
